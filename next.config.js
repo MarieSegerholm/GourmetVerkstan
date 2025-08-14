dc04f180b303d5530e599ab565ca27ac453989b9
MarieSegerholm <marie.segerholm1@gmail.com>
2025-08-14 13:34:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['sv', 'en'],
-    defaultLocale: 'sv',
-  },
+  i18n: { locales: ['sv', 'en'], defaultLocale: 'sv' }
 };
-
 export default nextConfig;
